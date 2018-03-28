cfcdca20f38a6550a9a3703bab19e3887d5134e8
Ronald Troyer <ron.troyer@gmail.com>
2018-03-28 16:01:51 -0400

    Add "of" to the list of common words

--- fml.js
+++ fml.js
@@ -495,7 +495,7 @@ javascript: (function () {
       },
       cleanTitle: function (titleStr) {
         titleStr = fmlApp.helpers.exceptionClean(titleStr); /* For movie specific cleanups that are bound to happen */
-        titleStr = titleStr.replace(/\b(a|an|the)\b/i, ''); /* Remove articles */
+        titleStr = titleStr.replace(/\b(a|an|the|of)\b/i, ''); /* Remove articles and common words */
         titleStr = titleStr.replace(/\d+$/, '').replace(/:.*/, ''); /* Try to make sequels consistent */
         titleStr = titleStr.split(' ').slice(0, 2).join(''); /* Take the first two words */
         titleStr = titleStr.replace(/\W/g, '').toLowerCase(); /* Cleanup */
